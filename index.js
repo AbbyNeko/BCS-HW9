c4fdc8058c07283ee23296b72e7d69c01f75f505
Abigail Limpioso <alimpioso@gmail.com>
2020-05-30 21:53:08 -0400

    added more questions and sections. Tested prompts and this is working. Need to add more template content

--- index.js
+++ index.js
@@ -22,15 +22,63 @@ inquirer
             message: "What are the technologies used? Please list them out and separate them out using commas.",
             name: "techused"
         }, {
-            message: "How would a user use this project?",
+            message: "How do you use this project?",
             name: "usage"
+        }, {
+            message: "How would users contribute to the project?",
+            name: "contributing"
+        }, {
+            message: "How do you test this project?",
+            name: "testing"
         }
-    ]).then(function({ githubuser, projectname, purpose, installation, techused, usage }) {
+    ]).then(function(answers) {
 
-        const fileContents = `Test`;
+        let readMeContents = createReadMeContents(answers);
 
-        writeFileAsync("./output/README.md", fileContents).then(function() {
+        writeFileAsync("./output/README.md", readMeContents).then(function() {
             console.log("Success");
         }).catch(err => console.error(err));
 
     });
+
+    function createReadMeContents({ githubuser, projectname, purpose, installation, techused, usage, contributing, testing }) {
+
+        //Create project name and Purpose of project
+        let fileContents = `# ${projectname}\n${purpose}\n\n`;
+
+        //Creates Table of Contents
+        fileContents += `##Table of Contents\n`;
+
+        //If installation question is answered with N/A, no installation section wil be added and it will not be in table of contents
+        if(installation != 'N/A') {
+        fileContents += `* Installation\n* Usage\n* Technologies Used\n* License\n* Contributing\n* Tests\n* Questions`;
+        fileContents += `## Installation\n${installation}\n\n`;
+        } else {
+            //Creating table of contents without Installation section
+            fileContents += `* Usage\n* Built With\n* License\n* Contributing\n* Tests\n* Questions`;
+            fileContents += `\n\n`;
+        }    
+
+        //Creates bulleted list of Tech Used
+        let techArr = techused.split(',');
+
+        fileContents += `## Usage\n${usage}\n\n## Built With\n`; 
+        
+        techArr.forEach(element => fileContents += `* ${element}\n`);
+
+        //Licensing
+        fileContents += `\n## License\n[MIT](https://choosealicense.com/licenses/mit/)`;
+
+        //Contributing Section
+        fileContents += `\n## Contributing\n`;
+
+        //Tests Section
+        fileContents += `\n## Tests\n`;
+
+        //Questions Section
+        fileContents += `\n## Questions\n`;
+
+
+        return fileContents;
+
+    }
---
+++ output/README.md
@@ -0,0 +1,25 @@
+# Weather Dashboard
+To show current weather conditions by city name
+
+##Table of Contents
+* Usage
+* Built With
+* License
+* Contributing
+* Tests
+* Questions
+
+## Usage
+By typing in a city name and pressing search.
+## Built With
+* HTML
+* CSS
+* JavaScript
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+## Contributing
+
+## Tests
+
+## Questions
